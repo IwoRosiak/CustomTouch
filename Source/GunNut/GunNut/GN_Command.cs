72f8744222b3352480c0a295a52b6ea051b5ec38
QarsoonMeel <qarsoonmeel@gmail.com>
2021-07-20 15:41:30 +0100

    Attachments are not visible on command buttons

---
+++ Source/GunNut/GunNut/GN_Command.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace GunNut
+{
+    public class GN_Command : Command_VerbTarget
+    {
+        protected override void DrawIcon(Rect rect, Material buttonMat, GizmoRenderParms parms)
+        {
+            base.DrawIcon(rect, buttonMat, parms);
+            rect.position += new Vector2(this.iconOffset.x * rect.size.x, this.iconOffset.y * rect.size.y);
+
+
+            if (verb.EquipmentSource.TryGetComp<GN_ThingComp>() != null)
+            {
+                var weapon = verb.EquipmentSource.TryGetComp<GN_ThingComp>();
+                foreach (var slot in weapon.Slots)
+                {
+                    if (slot.attachment != null)
+                    {
+                        Widgets.DrawTextureFitted(rect, slot.attachment.onWeaponGraphic.Graphic.MatSingle.mainTexture,
+                            this.iconDrawScale * 0.85f, this.iconProportions, this.iconTexCoords);
+                    }
+                }
+
+
+            }
+
+        }
+    }
+}
--- Source/GunNut/GunNut/HarmonyPatches.cs
+++ Source/GunNut/GunNut/HarmonyPatches.cs
@@ -244,4 +244,55 @@ namespace GunNut
             return false;
         }
     }
+
+    [HarmonyPatch(typeof(VerbTracker), "CreateVerbTargetCommand")]
+
+    public static class TestPatch1
+    {
+
+        [HarmonyPrefix]
+        public static bool GN_GetDamageAmount_PostFix(VerbTracker __instance, Thing ownerThing, Verb verb, ref Command_VerbTarget __result)
+        {
+            GN_Command command_VerbTarget = new GN_Command();
+            ThingStyleDef styleDef = ownerThing.StyleDef;
+            command_VerbTarget.defaultDesc = ownerThing.LabelCap + ": " + ownerThing.def.description.CapitalizeFirst();
+            command_VerbTarget.icon = ((styleDef != null && styleDef.UIIcon != null) ? styleDef.UIIcon : ownerThing.def.uiIcon);
+            command_VerbTarget.iconAngle = ownerThing.def.uiIconAngle;
+            command_VerbTarget.iconOffset = ownerThing.def.uiIconOffset;
+            command_VerbTarget.tutorTag = "VerbTarget";
+            command_VerbTarget.verb = verb;
+            if (verb.caster.Faction != Faction.OfPlayer)
+            {
+                command_VerbTarget.Disable("CannotOrderNonControlled".Translate());
+            }
+            else if (verb.CasterIsPawn)
+            {
+                string reason;
+                if (verb.CasterPawn.WorkTagIsDisabled(WorkTags.Violent))
+                {
+                    command_VerbTarget.Disable("IsIncapableOfViolence".Translate(verb.CasterPawn.LabelShort, verb.CasterPawn));
+                }
+                else if (!verb.CasterPawn.drafter.Drafted)
+                {
+                    command_VerbTarget.Disable("IsNotDrafted".Translate(verb.CasterPawn.LabelShort, verb.CasterPawn));
+                }
+                else if (verb is Verb_LaunchProjectile)
+                {
+                    Apparel apparel = verb.FirstApparelPreventingShooting();
+                    if (apparel != null)
+                    {
+                        command_VerbTarget.Disable("ApparelPreventsShooting".Translate(verb.CasterPawn.Named("PAWN"), apparel.Named("APPAREL")).CapitalizeFirst());
+                    }
+                }
+                else if (EquipmentUtility.RolePreventsFromUsing(verb.CasterPawn, verb.EquipmentSource, out reason))
+                {
+                    command_VerbTarget.Disable(reason);
+                }
+            }
+            __result = command_VerbTarget;
+            return false;
+
+        }
+    }
+
 }
